7e791b6a0d6d7c96ba61943ca7f1c92bcbf14a0e
NiiyaDaiki <56012694+NiiyaDaiki@users.noreply.github.com>
2021-01-21 02:54:36 +0900

    preflight request作成

--- public/index1.js
+++ public/index1.js
@@ -1,10 +1,32 @@
+//  simple request(GET)
 const getButton = document.getElementById('get');
-
 getButton.addEventListener('click', event => {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://112ff805688f.ngrok.io', true);
+    xhr.open('GET', 'http://67ef05605188.ngrok.io', true);
     xhr.withCredentials = true;
     xhr.send(null);
+})
+
+// simple request(POST)
+const postButton = document.getElementById('post');
+postButton.addEventListener('click', event => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://67ef05605188.ngrok.io/simple', true);
+    xhr.withCredentials = true;
+    xhr.send(null);
+})
+
+
+
+// preflight request(POST)
+const preFlightPostButton = document.getElementById('preflight');
+preFlightPostButton.addEventListener('click', event => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://67ef05605188.ngrok.io/preflight', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.withCredentials = true;
+    xhr.send();
+
 })
 
 // async/awaitを利用した書き方
